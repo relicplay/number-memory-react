04a9919293d0235df65f1b730ea43b87400da155
Fredrik Pihlgren <fp@relicplay.com>
2023-01-15 15:35:39 +0100

    Added all the original DOM

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,44 @@ function App() {
 
         <Start level={level} changeLevel={changeLevel} />
 
+        <section className="gamescreen">
+
+            <article id="randomdisplay"></article>
+
+            <article className="controls">
+                <h1 id="resultmessage"></h1>
+
+                <input type="text"
+                id="inputfield"
+                placeholder="Enter correct number sequence..."
+                autoComplete="off"
+                /*
+                onselectstart="return false"
+                oncut="return false"
+                onCopy="return false"
+                onpaste="return false"
+                ondrag="return false"
+                ondrop="return false"
+                */
+                />
+    
+                <div className="buttonwrapper">
+                    <button className="button" id="retrybutton">Retry</button>
+                    <button className="button" id="resetbutton">Reset</button>
+                    <button className="button" id="nextbutton">Next</button>
+                </div>
+            </article>
+
+            <article className="loadingscreen">
+                <div className="loader">
+                    <span id="counter"></span>
+                </div>
+                <span className="loadertext">Memorize the numbers!</span>
+                <button className="button" id="skipbutton">Skip</button>
+            </article>
+
+        </section>
+
         </main>
         <Footer />
     </div>
