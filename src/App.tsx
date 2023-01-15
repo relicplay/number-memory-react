69bd55bb770ee3c0f3bcd9281efa32325314dd83
Fredrik Pihlgren <fp@relicplay.com>
2023-01-15 13:18:37 +0100

    Added initial DOM & removed boilerplate logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 import Button from './components/Button';
@@ -7,8 +6,31 @@ import Button from './components/Button';
 
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
+        <header>
+          <h1 className="headertitle">Memorize</h1>
+          <h1 className="headertitle" id="headertitle-second">Number</h1>
+          <h1 className="headertitle" id="headertitle-third">Sequence</h1>
+        </header>
         <Button/>
+
+        <main>
+
+        <section className="startscreen">
+            <button id="startbutton" className="button">Start</button>
+            <div className="slidercontainer">
+                <h1>Begin at Level:</h1>
+                <input type="range" min="1" max="100" value="1" className="slider" id="levelslider" step="1" autoComplete="off" onChange={() => {console.log('value changed');}} />
+                <h1 id="leveldisplay"></h1>
+            </div>
+        </section>
+
+        </main>
+        <footer>
+        <p className="byline">
+            Created by Fredrik Pihlgren, 2023.
+        </p>
+        </footer>
     </div>
   );
 }
