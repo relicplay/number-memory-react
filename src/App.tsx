e0dd47b12a4ad1f70808c9d2f2155cb75c2c3ac5
Fredrik Pihlgren <fp@relicplay.com>
2023-01-15 13:33:31 +0100

    Added footer + header components. Added slider script

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
-import Button from './components/Button';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 
 function App() {
+
+  const [level, setLevel] = useState(1);
+
+  const changeLevel = (value: string) => {
+    setLevel(Number(value));
+  }
+
   return (
     <div className="wrapper">
-        <header>
-          <h1 className="headertitle">Memorize</h1>
-          <h1 className="headertitle" id="headertitle-second">Number</h1>
-          <h1 className="headertitle" id="headertitle-third">Sequence</h1>
-        </header>
-        <Button/>
-
+        <Header />
         <main>
 
         <section className="startscreen">
             <button id="startbutton" className="button">Start</button>
             <div className="slidercontainer">
                 <h1>Begin at Level:</h1>
-                <input type="range" min="1" max="100" value="1" className="slider" id="levelslider" step="1" autoComplete="off" onChange={() => {console.log('value changed');}} />
+                <input type="range" min="1" max="100" value={level} className="slider" id="levelslider" step="1" autoComplete="off" onChange={(e) => {changeLevel(e.target.value);}} />
                 <h1 id="leveldisplay"></h1>
             </div>
         </section>
 
         </main>
-        <footer>
-        <p className="byline">
-            Created by Fredrik Pihlgren, 2023.
-        </p>
-        </footer>
+        <Footer />
     </div>
   );
 }
