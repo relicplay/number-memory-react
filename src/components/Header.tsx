0e5350732e8ac2f747a488a0f6963c5de3c5a59f
Fredrik Pihlgren <fp@relicplay.com>
2023-02-11 21:15:51 +0100

    Removed the "2" previously added for testing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () =>
     <header>
         <h1 className="headertitle">Memorize</h1>
         <h1 className="headertitle" id="headertitle-second">Number</h1>
-        <h1 className="headertitle" id="headertitle-third">Sequence 2</h1>
+        <h1 className="headertitle" id="headertitle-third">Sequence</h1>
     </header>
 ;
 
